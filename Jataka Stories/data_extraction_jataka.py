c0867889013eb311eec1001e7f5ff18fe10575ce
刘淑文 <LancasterLiu_cryp@163.com>
2024-07-09 16:28:35 +0800

    restructure&add 六度集經

---
+++ Jataka Stories/data_extraction_jataka.py
@@ -0,0 +1,68 @@
+# 数据获取
+# coding: utf-8
+# author:liushuwen
+# creation:2024-7-
+
+import os
+import time
+from bs4 import *
+from requests import *
+from pandas import *
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+driver_path=r'D:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'
+options = Options()
+options.headless = True
+options.add_experimental_option("detach",True)
+driver=webdriver.Chrome(driver_path,options=options)
+
+chinese_texts = []
+english_texts = []
+i=1
+while i<92:
+    link="https://jatakastories.div.ed.ac.uk/stories-in-text/liudu-ji-jing-t152-%d"%i
+    # headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
+    # r=get(link,headers=headers)
+    # 找到文本元素
+    driver.get(link)
+    # 英语
+    translation_element = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "translation-text"))
+    )
+    translation_text = translation_element.text.strip()
+    # translation_text = soup.find('div',id="translation-text")
+    english_texts.append(translation_text)
+
+    # 中文
+    edition_element = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.ID, "edition-text"))
+    )
+    edition_text = edition_element.text.strip()
+    chinese_texts.append(edition_text)
+    if i==73:
+        i=81
+    else:
+        i+=1
+
+
+# soup=BeautifulSoup(r.text,"html.parser"
+# # print(soup)
+# print(english_texts)
+
+# edition_text = soup.find('div', id='edition-text')
+
+# print(edition_text)
+
+
+
+# 保存
+df = DataFrame({'Sanskrit': chinese_texts, 'english': english_texts})
+csv_filename = os.path.join("Āryaśūra's Jātakamālā" + ".csv")
+df.to_csv(csv_filename, index=False)
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 ## introduction
 This is a data extraction based of online content using python(spider).
 ## structure
-1. without en.csv lists all the parts without english translation.
-2. the forlders list all the data from the website with both chinese and english translation, stored with csv format.
-3. data_extraction.py is the original file to obtain all the data.
+- ctext
+  - **without en.csv** lists all the parts without english translation.
+  - **data_extraction_ctext.py** is the original file to obtain all the data.
+  - the forlders list all the data from the website with both Chinese and English translation, stored with csv format.
+- Jataka Stories
+  - **data_extraction_jataka.py** is the original file to obtain all the data.
+  - **the csv files** list all the data from the website with both Chinese/Sanskrit/Pali and English translation
