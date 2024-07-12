0918a48446f3e35a36f7b607a76390d87f3ff1d5
刘淑文 <LancasterLiu_cryp@163.com>
2024-07-12 11:48:03 +0800

    sanskrit&pali

--- Jataka Stories/data_extraction_jataka.py
+++ Jataka Stories/data_extraction_jataka.py
@@ -16,6 +16,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support import expected_conditions as EC
 driver_path=r'D:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe'
 options = Options()
@@ -26,46 +27,40 @@ driver=webdriver.Chrome(driver_path,options=options)
 chinese_texts = []
 english_texts = []
 i=1
-while i<41:
-    link="https://jatakastories.div.ed.ac.uk/stories-in-text/avadanasataka-%d"%i
-    # headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"}
-    # r=get(link,headers=headers)
-    # 找到文本元素
-    driver.get(link)
-    # 英语
-    translation_element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.ID, "translation-text"))
-    )
-    translation_text = translation_element.text.strip()
-    # translation_text = soup.find('div',id="translation-text")
-    english_texts.append(translation_text)
+# for i in [1,387]:
+while i<548:
+    link = f"https://jatakastories.div.ed.ac.uk/stories-in-text/jatakatthavannana-{i}"
+    try:
+        driver.get(link)
+        # 英语
+        translation_element = WebDriverWait(driver, 6).until(
+            EC.presence_of_element_located((By.ID, "translation-text"))
+        )
+        translation_text = translation_element.text.strip()
+        english_texts.append(translation_text)
 
-    # 中文
-    edition_element = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.ID, "edition-text"))
-    )
-    edition_text = edition_element.text.strip()
-    chinese_texts.append(edition_text)
-    # i+=1
-    if i==20:
-        i=22
-    elif i==9:
-        i=11
-    else:
-        i+=1
-
-
-# soup=BeautifulSoup(r.text,"html.parser"
-# # print(soup)
-# print(english_texts)
-
-# edition_text = soup.find('div', id='edition-text')
-
-# print(edition_text)
-
+        # 中文
+        edition_element = WebDriverWait(driver, 6).until(
+            EC.presence_of_element_located((By.ID, "edition-text"))
+        )
+        edition_text = edition_element.text.strip()
+        chinese_texts.append(edition_text)
+    except TimeoutException:
+        print(f"Timeout occurred for link: {link}")
+        pass
+    except Exception as e:
+        # print(f"An error occurred: {e}")
+        pass
+    i += 1
+    # if i==1127:
+    #     i=1231
+    # elif i==1283:
+    #     i=1317
+    # else:
+    #     i+=1
 
 
 # 保存
-df = DataFrame({'Sanskrit': chinese_texts, 'english': english_texts})
-csv_filename = os.path.join("Jataka Stories\sanskrit\Avadānaśataka.csv")
+df = DataFrame({'Pali': chinese_texts, 'english': english_texts})
+csv_filename = os.path.join("Jataka Stories\Pali\Jātakatthavaṇṇanā.csv")
 df.to_csv(csv_filename, index=False)
